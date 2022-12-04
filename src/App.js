cf9c626ca020e3dc7356aa268ce96235a6e17237
Ananya Nair <an.nair1702@gmail.com>
2022-12-05 03:08:22 +0530

    Added local storage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import './App.css';
 
 function App() {
 
-  // state var to store tasks in array and initialised with empty array
-  const [list, setList]=useState([]);
+  const [list, setList] = useState(() => {
+    // get the list from localstorage
+    const savedTodos = localStorage.getItem("list");
+    // if there are todos stored
+    if (savedTodos) {
+      // return the parsed the JSON object back to a javascript object
+      return JSON.parse(savedTodos);
+      // otherwise
+    } else {
+      // return an empty array
+      return [];
+    }
+  });
 
-  // state var to take what we fill in input
+  // state var to store tasks in array and initialised with empty array
+  // const [list, setList]=useState([]);
+
+  // state var to keep track of value inserted in input
   const[input, setInput]=useState("");
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
 
 
   // creating add todo function which takes in todo
@@ -49,9 +67,11 @@ function App() {
       />
       <button className='addbtn' onClick={()=> addTodo(input)}>Add</button>
       </div>
+
+      {/* create a ul to hold all of the list items  */}
       <ul>
       {/* taking all todos in array and mapping them */}
-      {/* map each todo to list using key as the id */}
+      {/* map each todo to list(creates new li for every todo) using key as the id */}
         {list.map((todo) =>(
           <li key={todo.id}>
           {/* diaply the actual toda as object todo. property todo */}
